41e04a56d1f0722140d2118c02e4a58165d6cccb
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2015-11-10 16:30:53 +0100

    Fix label rendering and pep8 widgets.py

--- collective/arboreal/widgets.py
+++ collective/arboreal/widgets.py
@@ -1,49 +1,78 @@
+""" Arboreal widgets """
 from AccessControl import ClassSecurityInfo
-from Products.Archetypes.Widget import MultiSelectionWidget, \
-        SelectionWidget, TypesWidget
+from Products.Archetypes.Widget import MultiSelectionWidget,\
+    SelectionWidget, TypesWidget
 from Products.Archetypes.Registry import registerWidget
 
+
 class MultiTreeSelectionWidget(MultiSelectionWidget):
+    """ MultiTreeSelectionWidget """
     _properties = MultiSelectionWidget._properties.copy()
     _properties.update({
-        'macro':'abr_multiselection_widget',
-        'format':'checkbox', # One of checkbox, select, collapsed or finder
+        'macro': 'abr_multiselection_widget',
+        'format': 'checkbox',  # One of checkbox, select, collapsed or finder
         })
 
     security = ClassSecurityInfo()
-    
-registerWidget(MultiTreeSelectionWidget,
-               title='Multi Tree Selection',
-               description=('Selection widget that allows you to select multiple items from an Arboreal tree.'),
-               used_for=('Products.Arboreal.field.MultiArborealField',)
-               )
+
+    security.declarePublic('render_own_label')
+
+    def render_own_label(self):
+        """ We do not want to render our own label thank you """
+        return False
+
+registerWidget(
+    MultiTreeSelectionWidget,
+    title='Multi Tree Selection',
+    description=('Selection widget that allows you to select multiple items'
+                 ' from an Arboreal tree.'),
+    used_for=('Products.Arboreal.field.MultiArborealField',)
+    )
+
 
 class TreeSelectionWidget(SelectionWidget):
+    """ TreeSelectionWidget """
     _properties = MultiSelectionWidget._properties.copy()
     _properties.update({
-        'macro':'abr_selection_widget'
+        'macro': 'abr_selection_widget'
         })
 
     security = ClassSecurityInfo()
-    
-registerWidget(TreeSelectionWidget,
-               title='Tree Selection',
-               description=('Selection widget that allows you to select one item from an Arboreal tree.'),
-               used_for=('Products.Arboreal.field.SingleArborealField',)
-               )
+
+    security.declarePublic('render_own_label')
+
+    def render_own_label(self):
+        """ We do not want to render our own label thank you """
+        return False
+
+registerWidget(
+    TreeSelectionWidget,
+    title='Tree Selection',
+    description=('Selection widget that allows you to select one'
+                 ' item from an Arboreal tree.'),
+    used_for=('Products.Arboreal.field.SingleArborealField',))
+
 
 class ArborealLabledWidget(TypesWidget):
+    """" ArborealLabledWidget """
     _properties = TypesWidget._properties.copy()
-    _properties.update({'macro':'arboreallabled_widget'})
+    _properties.update({'macro': 'arboreallabled_widget'})
 
     security = ClassSecurityInfo()
 
+    security.declarePublic('render_own_label')
+
+    def render_own_label(self):
+        """ We do not want to render our own label thank you """
+        return False
+
     security.declarePublic('process_form')
+
     def process_form(self, instance, field, form, empty_marker=None,
                      emptyReturnsMarker=False):
         """Basic impl for form processing in a widget"""
         lable = form.get(field.getName()+'_lable', empty_marker)
-        text  = form.get(field.getName()+'_value', empty_marker)
+        text = form.get(field.getName()+'_value', empty_marker)
         value = (lable, text)
 
         if value is empty_marker:
@@ -52,9 +81,9 @@ class ArborealLabledWidget(TypesWidget):
             return empty_marker
         return value, {}
 
-registerWidget(ArborealLabledWidget,
-               title='Arboreal Labled widget',
-               description=(
-                   'This field has a text entry with a single Arboreal selection.'),
-               used_for=('Products.Arboreal.field.ArborealLabledField',)
-               )
+registerWidget(
+    ArborealLabledWidget,
+    title='Arboreal Labled widget',
+    description=(
+        'This field has a text entry with a single Arboreal selection.'),
+    used_for=('Products.Arboreal.field.ArborealLabledField',))
